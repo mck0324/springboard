470e97f61398a643fc0e93773bd33e53b7ed255e
mck0324 <dch3030@naver.com>
2023-06-07 23:54:51 +0900

    JPA기능을 assertThat으로 테스트해보기

---
+++ src/test/java/com/spring/springboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.spring.springboard.repository;
+
+import com.spring.springboard.config.JpaConfig;
+import com.spring.springboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,@Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_when_thenWorksFind() {
+        //Given
+
+        //When
+        List<Article> articles = articleRepository.findAll();
+        //Then
+        assertThat(articles).isNotNull().hasSize(0);
+    }
+}
