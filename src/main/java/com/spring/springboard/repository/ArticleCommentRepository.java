59925f851b0b5ef941d8644ea023d63f065070c2
mck0324 <dch3030@naver.com>
2023-06-21 23:54:10 +0900

    hal적용
    localhost:8080/api로 들어가게되면 HAL Explorer실행가능

--- src/main/java/com/spring/springboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/spring/springboard/repository/ArticleCommentRepository.java
@@ -4,6 +4,6 @@ import com.spring.springboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource()
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
