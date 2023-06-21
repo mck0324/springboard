46fcba796041702edb27f9b008ce96c2f4d180e1
mck0324 <dch3030@naver.com>
2023-06-21 23:48:23 +0900

    @RepositoryRestResource() 사용해보기

--- src/main/java/com/spring/springboard/repository/ArticleRepository.java
+++ src/main/java/com/spring/springboard/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.spring.springboard.repository;
 
 import com.spring.springboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource()
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
