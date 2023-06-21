cfe34ed2df8db2212c40ccf533219d49003409ee
mck0324 <dch3030@naver.com>
2023-06-22 00:18:42 +0900

    API호출결과
    .andDo(print())

--- src/test/java/com/spring/springboard/controller/DataRestTest.java
+++ src/test/java/com/spring/springboard/controller/DataRestTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,7 +29,8 @@ public class DataRestTest {
         //When & Then
         mvc.perform(get("/api/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
 
     }
 }
