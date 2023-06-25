42645bb94e44fd383b2dfd94595fc6f85fc0cea7
mck0324 <dch3030@naver.com>
2023-06-25 22:01:53 +0900

    Querydsl 적용 및 Article title 기본적인 검색 기능

--- src/main/java/com/spring/springboard/repository/ArticleRepository.java
+++ src/main/java/com/spring/springboard/repository/ArticleRepository.java
@@ -2,9 +2,16 @@ package com.spring.springboard.repository;
 
 import com.spring.springboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>
+//      QuerydslPredicateExecutor<Article>,로 인해 해당 되는 Article Entity에 대한 기본적인 검색기능은 끝남
+//        QuerydslBinderCustomizer<QArticle>
+{
 
 }
